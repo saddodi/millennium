ee2cbd355538c89fa386547efb4ae7b4b7e9bd93
Gurban Haydarov <ghaydarov@gmail.com>
2012-01-28 03:27:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Millennium::Application.config.secret_token = 'ef9655b93b3675dfc8030a2dc02970305beb191dfeadc6dfb67b37744bca9294e52f4b5a0b7ba20ab623e2fbb3e1380eb03dd3f84951dfd88c926f0a25ff3501'
