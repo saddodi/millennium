de54342dba1c93627d0460f26463d18e45c18ae2
Gurban Haydarov <ghaydarov@gmail.com>
2012-02-01 20:28:43 -0500

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,13 +15,13 @@ Millennium::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-   config.assets.manifest = Rails.root.join("deploy/assets")
+  # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
